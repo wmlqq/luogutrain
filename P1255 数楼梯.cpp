9bd2f2b3edd7079205ecd8744ef824598b3967ca
wmlqq <2118602087@qq.com>
2025-03-26 18:09:31 +0800

    Add files via upload
    # P1255 数楼梯
    
    ## 题目描述
    
    楼梯有 $N$ 阶，上楼可以一步上一阶，也可以一步上二阶。
    
    编一个程序，计算共有多少种不同的走法。
    
    ## 输入格式
    
    一个数字，楼梯数。
    
    ## 输出格式
    
    输出走的方式总数。
    
    ## 输入输出样例 #1
    
    ### 输入 #1
    
    ```
    4
    ```
    
    ### 输出 #1
    
    ```
    5
    ```
    
    ## 说明/提示
    
    - 对于 $60\%$ 的数据，$N \leq 50$；   
    - 对于 $100\%$ 的数据，$1 \le N \leq 5000$。

---
+++ P1255 数楼梯.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int>first(1500,0);
+    vector<int>second(1500,0);
+    second[0]=1;
+    int N;
+    cin>>N;
+    while(N!=0){
+        int temp=0;
+        for(int i=0;i<1500;i++){
+            int add=first[i]+second[i]+temp;
+            first[i]=second[i];
+            second[i]=add%10;
+            temp=add/10;
+        }
+        N--;
+    }
+    int i;
+    for( i=second.size()-1;i>=0;i--){
+        if(second[i]!=0)break;
+    }
+    while(i>=0){
+        cout<<second[i];
+        i--;
+    }
+    return 0;
+
+}
